9f11cc0c2cdb3be18258e7ea187eb15674422b63
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-09-16 18:45:24 -0500

    updated the css file

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,22 @@
-export default function MusicPlayer() {
-  return <p>Music player goes here</p>;
+import CurrentlyPlaying from './CurrentlyPlaying';
+import Playlist from './Playlist';
+
+function MusicPlayer() {
+  return (
+    <div className="min-h-screen bg-black p-4 text-white md:p-8">
+      <div className="flex flex-col gap-4 md:flex-row">
+        {/* Left Column: Currently Playing */}
+        <div className="w-full md:w-2/5">
+          <CurrentlyPlaying />
+        </div>
+
+        {/* Right Column: Playlist */}
+        <div className="w-full md:w-3/5">
+          <Playlist />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default MusicPlayer;
