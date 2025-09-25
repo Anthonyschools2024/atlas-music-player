983b79394ff20a51a2b69121e839449705b3d664
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-09-24 23:46:54 -0500

    added skeleton load

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { useState, useEffect } from 'react';
 import MusicPlayer from './components/MusicPlayer';
 import Footer from './components/Footer';
+import LoadingSkeleton from './components/LoadingSkeleton';
 import useDarkMode from './hooks/useDarkMode';
 import './index.css';
 
 function App() {
-  // This initializes the dark mode functionality
+  const [isLoading, setIsLoading] = useState(true);
   useDarkMode();
+
+  // Simulate a data fetch on initial component load
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Show skeleton for 3 seconds
+
+    // Clean up the timer when the component is unmounted
+    return () => clearTimeout(timer);
+  }, []); // The empty array ensures this effect runs only once
 
   return (
     <main className="bg-background">
-      <MusicPlayer />
-      <Footer />
+      {isLoading ? (
+        <LoadingSkeleton />
+      ) : (
+        <>
+          <MusicPlayer />
+          <Footer />
+        </>
+      )}
     </main>
   );
 }
