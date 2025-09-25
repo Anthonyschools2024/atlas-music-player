6978732b186b6aa0817ddd6cb6d79fa95d9fe2c5
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-09-24 23:02:38 -0500

    added the dark mode varible to the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import MusicPlayer from './components/MusicPlayer';
+import Footer from './components/Footer';
+import useDarkMode from './hooks/useDarkMode';
 import './index.css';
 
 function App() {
+  // Initialize the hook here to set the theme on initial page load
+  useDarkMode();
+
   return (
-    <main>
+    <main className="bg-background">
       <MusicPlayer />
+      <Footer />
     </main>
   );
 }
