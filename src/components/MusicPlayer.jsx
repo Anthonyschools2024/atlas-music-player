7e0745f9d03ebc63b1aa11508769745b5eb81325
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-09-24 23:07:03 -0500

    updated theme route to the main app file

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -3,14 +3,11 @@ import Playlist from './Playlist';
 
 function MusicPlayer() {
   return (
-    <div className="min-h-screen bg-background p-4 font-sans text-text-primary md:p-8">
+    <div className="bg-background p-4 font-sans text-text-primary md:p-8">
       <div className="mx-auto flex max-w-7xl flex-col gap-8 md:flex-row">
-        {/* Left Column: Currently Playing */}
         <div className="w-full md:w-2/5">
           <CurrentlyPlaying />
         </div>
-
-        {/* Right Column: Playlist */}
         <div className="w-full md:w-3/5">
           <Playlist />
         </div>
